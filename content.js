eb64d08717196a54d5fa748cd561dccd258efd1f
codebreaker09 <sanchezjm109@hotmail.com>
2024-10-29 12:28:02 -0400

    beta1

--- content.js
+++ content.js
@@ -3,32 +3,41 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'showPopup') {
     console.log('Showing popup modal...');
 
-    // This is our css, this is how our popup will look to the user
+    // This is our css, this is how our popup/modal will look to the user
     const modal = document.createElement('div');
+    modal.setAttribute('id', 'popup-container');
     modal.style.position = 'fixed';
     modal.style.top = '50%';
     modal.style.left = '50%';
     modal.style.transform = 'translate(-50%, -50%)';
     modal.style.backgroundColor = 'white';
-    modal.style.padding = '20px';
+    modal.style.padding = '25px';
     modal.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.5)';
     modal.style.zIndex = '10000';
     modal.style.textAlign = 'center';
+    modal.style.width = '500px';
+    modal.style.height = '450px';
 
     // This will add our gif to popup/
     const gif = document.createElement('img');
     gif.src = chrome.runtime.getURL('assets/explosion.gif');
-    gif.style.width = '200px'; // Only made it 200px wide, we can change that later
+    gif.style.width = '450px'; // Only made it 200px wide, we can change that later
+    gif.style.height = '400px';
 
     // Here we added a message to our popup
     const messageText = document.createElement('p');
+    messageText.style.fontSize = '20px';
+    messageText.style.margin = '8px';
+    messageText.style.fontWeight = '3px';
     messageText.textContent =
       "Time's up! You've been on this site for too long!";
 
     // added a close button to the popup, also added some css
     const closeButton = document.createElement('button');
     closeButton.textContent = 'Close';
-    closeButton.style.marginTop = '10px';
+    closeButton.style.marginBottom = '10px';
+    closeButton.style.borderRadius = '8px';
+    closeButton.style.width = '95px';
     closeButton.addEventListener('click', () => {
       document.body.removeChild(modal);
     });
